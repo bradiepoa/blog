1cc008e1ebec471400a07c0dab737c102ab89ea2
bradiepoa <bradiepoa7@gmail.com>
2022-01-16 23:49:59 +0300

    category changingd

--- mainweb/models.py
+++ mainweb/models.py
@@ -28,6 +28,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=250)
 	slug = models.SlugField(max_length=250, unique_for_date='publish')
 	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+	category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True,blank=True )
 	img = models.ImageField(upload_to='images/blog', blank=True, null=True)
 	body =models.TextField()
 	publish = models.DateTimeField(default=timezone.now)
