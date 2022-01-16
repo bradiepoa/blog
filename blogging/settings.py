b46f3e26aa59c56b88007389d8960a46e6212fab
bradiepoa <bradiepoa7@gmail.com>
2022-01-16 18:25:39 +0300

    static online configurations

--- blogging/settings.py
+++ blogging/settings.py
@@ -123,10 +123,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'static')
 
-]
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR, 'static')
+
+# ]
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
