44ea6a582aa00a5f669549a8de29f8d329ae1a40
samuels97 <kalumbasml@gmail.com>
2022-01-23 11:43:09 +0000

    nm

--- mainweb/admin.py
+++ mainweb/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import *
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import User, Group
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
 
 # Register your models here.
 @admin.register(Post)
@@ -15,4 +20,6 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Comment)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
